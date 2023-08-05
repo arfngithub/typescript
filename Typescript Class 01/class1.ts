7dedf4392ad0249bcd1850be4883d0a277f74ed3
AR_FN <141509966+arfngithub@users.noreply.github.com>
2023-08-05 20:50:00 +0500

    1st Class

---
+++ Typescript Class 01/class1.ts
@@ -0,0 +1,48 @@
+var userName = "Abdul Rehman";
+var message = `Hey Abu-Hurairah, I have started learning TYPESCRIPT.`;
+console.log(message);
+
+var userName = "Name";
+console.log(`My name is Name.`);
+
+var num1 = 5;
+var num2 = 10;
+var num3 = 15;
+var num4 = 20;
+var num5 = 25;
+var num6 = 30;
+var num7 = 35;
+var num8 = 40;
+var num9 = 45;
+var num10 = 50;
+
+var SUM = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log("Sum:",SUM);
+
+var Difference: number = num10 - num5;
+console.log("Difference:",Difference);
+
+var Product: number = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log("Product:", Product);
+
+var Number1: number = 100;
+var Number2 = 10;
+
+var divisionResult = Number1 / Number2;
+console.log("Division Result:", divisionResult);
+
+var a = 5;
+var b = 10;
+var c = 15;
+
+var result1 = a + b - c * a / b;
+console.log("Result 1:", result1);
+
+var result2 = (a + b) * c;
+console.log("Result 2:", result2);
+
+var result3 = a - (b + c) * a / b;
+console.log("Result 3:", result3);
+
+var result4 = a * (b - c) + a / b;
+console.log("Result 4:", result4);
